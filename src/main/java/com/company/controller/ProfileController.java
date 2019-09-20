81e65c04470450c2088d9ff178fbe14b8e7a83ea
sn4ntr <alex.miron1999@gmail.com>
2019-09-20 23:48:51 +0300

    ProfileController refactoring

--- src/main/java/com/company/controller/ProfileController.java
+++ src/main/java/com/company/controller/ProfileController.java
@@ -1,19 +1,14 @@
 package com.company.controller;
 
 import com.company.model.Post;
-import com.company.model.User;
 import com.company.service.PostService;
 import com.company.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 @Controller
 public class ProfileController {
@@ -29,9 +24,9 @@ public class ProfileController {
 
     @RequestMapping(value = "/profile", method = RequestMethod.GET)
     public ModelAndView profile() {
-        int userId = findUserId(userService.getAllUsers());
         ModelAndView modelAndView = new ModelAndView();
 
+        int userId = userService.findCurrentUserIdByEmail();
         if ("guest".equals(userService.getById(userId).getEmail())) {
             modelAndView.setViewName("redirect:/");
         } else {
@@ -45,47 +40,31 @@ public class ProfileController {
     public ModelAndView profile(@PathVariable("id") int id) {
         ModelAndView modelAndView = new ModelAndView();
 
-        int currentUserId = findUserId(userService.getAllUsers());
+        int currentUserId = userService.findCurrentUserIdByEmail();
         if (currentUserId != id && !"admin".equals(userService.getById(currentUserId).getEmail())) {
             modelAndView.setViewName("redirect:/profile/" + currentUserId);
         } else {
-            User user = userService.getById(id);
             modelAndView.setViewName("user/profile/profile");
-            modelAndView.addObject("user", user);
+            modelAndView.addObject("user", userService.getById(id));
         }
 
         return modelAndView;
     }
 
-    private int findUserId(List<User> users) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-
-        int userId = 0;
-        for (User u : users) {
-            if (auth.getName().equals(u.getEmail())) {
-                userId = u.getId();
-                return userId;
-            }
-        }
-
-        return userId;
-    }
-
     @RequestMapping(value = "/profile/post/{id}", method = RequestMethod.GET)
     public ModelAndView profilePost(@PathVariable("id") int id) {
+        int currentUserId = userService.findCurrentUserIdByEmail();
+        Post post = postService.getById(id);
+
         ModelAndView modelAndView = new ModelAndView();
 
-        int currentUserId = findUserId(userService.getAllUsers());
-        List<Post> posts = postService.getAllPosts();
-        for (Post p : posts) {
+        if (post.getUser().getId() != currentUserId && !"admin".equals(userService.getById(currentUserId).getEmail())) {
+            modelAndView.setViewName("redirect:/");
+        } else {
             modelAndView.setViewName("user/profile/post");
-
-            if (p.getUser().getId() != currentUserId && !"admin".equals(userService.getById(currentUserId).getEmail())) {
-                modelAndView.setViewName("redirect:/");
-            }
+            modelAndView.addObject("post", post);
         }
 
-        modelAndView.addObject("post", postService.getById(id));
         return modelAndView;
     }
 }
